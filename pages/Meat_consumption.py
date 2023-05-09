450841e622e2e58c8e7598bc0485e3aaf169ca89
zyankarli <scheifinger.karl@hotmail.com>
2023-05-09 17:24:30 +0200

    Update Meat_consumption.py
    fix syntax

--- pages/Meat_consumption.py
+++ pages/Meat_consumption.py
@@ -10,7 +10,9 @@ import time
 connection = connect(":memory:",
                      adapter_kwargs = {
                             "gsheetsapi": { 
-                            "service_account_info":  st.secrets["gcp_service_account"]
+                            "service_account_info":  {
+                                "type": st.secrets["gcp_service_account"]
+                            }    
                                     }
                                         }
                         )
