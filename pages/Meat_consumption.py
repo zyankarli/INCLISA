b55a8d6460a4a1f4c535dc060e4f914629f2397f
zyankarli <scheifinger.karl@hotmail.com>
2023-05-09 17:12:17 +0200

    Update Meat_consumption.py

--- pages/Meat_consumption.py
+++ pages/Meat_consumption.py
@@ -10,7 +10,7 @@ import time
 connection = connect(":memory:",
                      adapter_kwargs = {
                             "gsheetsapi": { 
-                            "service_account_info":  st.secrets["gcp_service_account"]["type"] 
+                            "service_account_info":  st.secrets["gcp_service_account"]
                                     }
                                         }
                         )
