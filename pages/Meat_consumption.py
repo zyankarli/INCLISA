9b383921b6a1742e6b8fdc6e2f7caf2503803184
zyankarli <scheifinger.karl@hotmail.com>
2023-05-12 14:59:25 +0200

    Update Meat_consumption.py
    push problem to future

--- pages/Meat_consumption.py
+++ pages/Meat_consumption.py
@@ -175,9 +175,10 @@ with st.form("Survey"):
     q2 = st.text_input("Why do you find this scenario to be the fairest?", key=2)
     q3 = st.radio("Which of the following aspects does best describe your main reason for your scenario selection?", accepted_answers2, key=3 )
     st.markdown('### Personal Questions')
-    q4 = st.selectbox("Which country are you from?",
-                      pd.read_csv("https://raw.githubusercontent.com/OxfordEconomics/CountryLists/master/countryList-UN.csv", 
-                                  lineterminator='\n',skiprows=1, encoding="ascii"))
+    #TODO fix country selection
+    # q4 = st.selectbox("Which country are you from?",
+    #                  pd.read_csv("https://raw.githubusercontent.com/OxfordEconomics/CountryLists/master/countryList-UN.csv", 
+    #                              lineterminator='\n',skiprows=1, encoding="ascii"))
     q5 = st.selectbox("What type of organisation do you work for?",
                       ("Government", "Research or academic organisation", "Non-governmental organisation", "Interational organisation", "Private Sector", "Other"))
     #TODO: add conditional pop-up for st text with other
