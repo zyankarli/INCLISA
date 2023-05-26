ed473574648ec202d504917b42fc48689ed68e1c
zyankarli <scheifinger.karl@hotmail.com>
2023-05-26 14:17:41 +0200

    Update Results.py
    improve graph, include update button

--- pages/Results.py
+++ pages/Results.py
@@ -46,30 +46,62 @@ def run_query(query):
     return rows
 
 #Get data
-#TODO: only reload graph on click
-df = pd.DataFrame(run_query(f'SELECT * FROM "{sheet_url}"'))
+##define data wrangling function
+def wrangle_data():
+    df = pd.DataFrame(run_query(f'SELECT * FROM "{sheet_url}"'))
 #rename columns
-df.columns= ['scen_meat', 'scen_meat_feedback', 'scen_meat_reason', 
+    df.columns= ['scen_meat', 'scen_meat_feedback', 'scen_meat_reason', 
             'scen_tran', 'scen_tran_feedback', 'scen_tran_reason', 
             'scen_buil', 'scen_buil_feedback', 'scen_buil_reason', 
             'scen_gdp', 'scen_gdp_feedback', 'scen_gdp_reason', 
              'meat_consumption', 'air_travel', 'housing_space','country', 'organisation', 'education', 'age','gender', 'sector','iam','timestamp']
 
-#wrangle data
-to_plot = df[['scen_meat', 'scen_tran', 'scen_buil', 'scen_gdp']]
-#change wide to long
-to_plot = pd.melt(to_plot, var_name="Scenario", value_name="Value")
-#drop "-"
-to_plot = to_plot[to_plot.Value != "-"]
+    #wrangle data
+    to_plot = df[['scen_meat', 'scen_tran', 'scen_buil', 'scen_gdp']]
+    #rename scenarios
+    to_plot = to_plot.rename(columns={'scen_meat':"Food",
+                                      'scen_tran':"Mobility",
+                                      'scen_buil':'Housing',
+                                      'scen_gdp':"Economic Activity"})
 
+    #change wide to long
+    to_plot = pd.melt(to_plot, var_name="Scenario", value_name="Value")
+    #drop "-"
+    to_plot = to_plot[to_plot.Value != "-"]
+    return to_plot
+
+to_plot = wrangle_data()
+
+#TODO: only reload graph on click
+if st.button('Click here to update the graph.'):
+    wrangle_data()      
 
 fig = px.bar(to_plot, x ='Value', color="Scenario",barmode='group',
               labels={
-                     "Value": "Scenario",
+                     "Value": "",
                      'count': "Count"
                 },
                 #TODO automise random order
-                title="Chosen climate mitigation scenarios per sector")
+                title="Results: Climate mitigation scenarios per sector")
+
+#Size
+plot_width=600 
+plot_height= plot_width * 0.75 
+
+
+fig.update_layout(
+    legend = dict(
+        title_text = "Sector",
+        ),
+        width = plot_width,
+        height = plot_height
+    )
+
+#Deactivate zoom/ True = deactivated
+#Disable zoom feature
+fig.layout.xaxis.fixedrange = False
+fig.layout.yaxis.fixedrange = False
+
 
 st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
@@ -77,6 +109,4 @@ st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
 
 #adjust layout
-#fig.update_layout(height = 600 ,width=700)
-
-
+#inclulde buttons ?https://plotly.com/python/custom-buttons/
